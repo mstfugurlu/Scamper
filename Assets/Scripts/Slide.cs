12454cf90145c00711471b4ae00f325db66fb58a
mstfugurlu <mustafa.t.ugurlu@gmail.com>
2022-02-21 14:33:19 +0300

    revised

--- Assets/Scripts/Slide.cs
+++ Assets/Scripts/Slide.cs
@@ -7,8 +7,13 @@ public class Slide : MonoBehaviour
 {
     //[SerializeField] private Transform obstacle;
     private Vector2 startTouch, slideDelta, secondTouch;
-    //public Action<Transform>RotateObstacle=delegate {  };
+    public Action<Transform>RotateObstacle;
 
+    
+
+    private void Start()
+    {
+    }
 
     private void Update()
     {
@@ -22,8 +27,11 @@ public class Slide : MonoBehaviour
             secondTouch = Input.mousePosition;
             slideDelta = secondTouch - startTouch;
             startTouch = secondTouch;
-            Debug.Log(slideDelta);
-            
+            //Debug.Log(slideDelta);
+            transform.Rotate(0,0,slideDelta.x/5);
+                     // transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0,slideDelta.x), 0.2f);
+                     Vector3 rotationVector = new Vector3(0, 0, slideDelta.x);
+                     Quaternion rotation=Quaternion.Euler(rotationVector);
             
         }
         
